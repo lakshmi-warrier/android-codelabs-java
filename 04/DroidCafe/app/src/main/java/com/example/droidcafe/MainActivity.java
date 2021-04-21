c408b2ec0fb2bdf82f18660f3477870ba6c4479a
lakshmi-warrier <lakshmi0105warrier@gmail.com>
2021-04-21 11:11:24 +0530

    4.1

--- 04/DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ 04/DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.droidcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void displayToast(String message) {
@@ -19,15 +53,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_SHORT).show();
     }
 
+
     public void showDonutOrder(View view) {
         displayToast(getString(R.string.donut_order_message));
     }
+
 
     public void showIceCreamOrder(View view) {
         displayToast(getString(R.string.ice_cream_order_message));
     }
 
+
     public void showFroyoOrder(View view) {
         displayToast(getString(R.string.froyo_order_message));
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        startActivity(intent);
     }
 }
